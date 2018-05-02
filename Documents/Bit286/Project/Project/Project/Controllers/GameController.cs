8bc0b5f44252f913d6f2b12bab33ba7de7b8607b
Roberto Ramirez <robertramirez658@outlook.com>
2018-05-02 04:32:14 -0700

    FirstGame and login

---
+++ Documents/Bit286/Project/Project/Project/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult FirstGame()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FirstGame(users m)
+        {
+          return RedirectToAction("Startup");
+        }
+    }
+}
