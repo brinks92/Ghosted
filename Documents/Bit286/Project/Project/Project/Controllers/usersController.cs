51d4783cf12601e96726034b00560bfbfe667775
Roberto Ramirez <robertramirez658@outlook.com>
2018-06-05 14:49:34 -0700

    2 controller one new page

--- Documents/Bit286/Project/Project/Project/Controllers/usersController.cs
+++ Documents/Bit286/Project/Project/Project/Controllers/usersController.cs
@@ -30,7 +30,7 @@ namespace Project.Controllers
 
 
                 case "Student":
-                    return RedirectToAction("StudentStart", "Game");
+                    return RedirectToAction("Welcome", "Game");
             }
 
             return View();
